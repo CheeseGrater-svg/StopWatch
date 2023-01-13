5a3a4d1ff3cf4ceb1bc88fe2a383e53890142356
IncogNEET <incogneet@vivaldi.net>
2023-01-13 10:08:49 +0900

    奥付の修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,104 +1,140 @@
 <script lang="ts">
-  import icon from "$lib/assets/stopwatch.png";
+	import icon from '$lib/assets/stopwatch.png';
 
-  enum STATE {
-    NEW,
-    RUNNING,
-    PAUSED,
-  }
+	enum STATE {
+		NEW,
+		RUNNING,
+		PAUSED
+	}
 
-  let state: STATE = STATE.NEW;
-  let startTime: number = 0;
-  let elapsedTime: number = 0;
-  let oldElapsedTime: number = 0;
-  let interval: number;
+	let state: STATE = STATE.NEW;
+	let startTime: number = 0;
+	let elapsedTime: number = 0;
+	let oldElapsedTime: number = 0;
+	let interval: number;
 
-  $: ms = String(elapsedTime % 1000).slice(-3, -1).padStart(2, '0');
-  $: s = String(Math.floor(elapsedTime / 1000) % 60).slice(-2).padStart(2, '0');
-  $: m = String(Math.floor((elapsedTime / 1000) / 60) % 60).slice(-2).padStart(2, '0');
-  $: h = String(Math.floor(((elapsedTime / 1000) / 60) / 60) % 60).slice(-2).padStart(2, '0');
+	$: ms = String(elapsedTime % 1000)
+		.slice(-3, -1)
+		.padStart(2, '0');
+	$: s = String(Math.floor(elapsedTime / 1000) % 60)
+		.slice(-2)
+		.padStart(2, '0');
+	$: m = String(Math.floor(elapsedTime / 1000 / 60) % 60)
+		.slice(-2)
+		.padStart(2, '0');
+	$: h = String(Math.floor(elapsedTime / 1000 / 60 / 60) % 60)
+		.slice(-2)
+		.padStart(2, '0');
 
-  const start = () => {
-    startTime = Date.now();
-    state = STATE.RUNNING;
-    interval = Number(setInterval(() => {
-      if (state === STATE.RUNNING) {
-        const endTime = Date.now();
-        elapsedTime = endTime - startTime + oldElapsedTime;
-      }
-    },));
-  };
+	const start = () => {
+		startTime = Date.now();
+		state = STATE.RUNNING;
+		interval = Number(
+			setInterval(() => {
+				if (state === STATE.RUNNING) {
+					const endTime = Date.now();
+					elapsedTime = endTime - startTime + oldElapsedTime;
+				}
+			})
+		);
+	};
 
-  const reset = () => {
-    elapsedTime = 0;
-    oldElapsedTime = 0;
-    state = STATE.NEW;
-    clearInterval(interval);
-  };
+	const reset = () => {
+		elapsedTime = 0;
+		oldElapsedTime = 0;
+		state = STATE.NEW;
+		clearInterval(interval);
+	};
 
-  const stop = () => {
-    state = STATE.PAUSED;
-    oldElapsedTime = elapsedTime;
-  };
+	const stop = () => {
+		state = STATE.PAUSED;
+		oldElapsedTime = elapsedTime;
+	};
 
-  const resume = () => {
-    startTime = Date.now();
-    state = STATE.RUNNING;
-
-  };
+	const resume = () => {
+		startTime = Date.now();
+		state = STATE.RUNNING;
+	};
 </script>
 
 <div class="relative mt-16">
-  <img src={icon} alt="watch icon" class="w-24 box-content relative p-2 peer z-10"/>
-  <div class="absolute inset-0 z-0 bg-gradient-to-r from-cyan-500 to-fuchsia-500 rounded-3xl blur-xl opacity-50"></div>
+	<img src={icon} alt="watch icon" class="peer relative z-10 box-content w-24 p-2" />
+	<div
+		class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-50 blur-xl"
+	/>
 </div>
 
-
-<h1 class="text-5xl font-black leading-none mt-6">StopWatch</h1>
+<h1 class="mt-6 text-5xl font-black leading-none">StopWatch</h1>
 <h2 class="text-base font-medium leading-relaxed">Coolest Web Stopwatch</h2>
 
 <div class="relative mt-16">
-  {#if state === STATE.NEW}
-  <button class="transition bg-white hover:bg-black hover:text-white text-black w-40 py-1 text-base leading-relaxed rounded-lg font-bold border border-white relative z-10 peer" on:click={start}>Start</button>
-  {/if}
-  {#if state === STATE.RUNNING}
-  <button class="transition bg-white hover:bg-black hover:text-white text-black w-40 py-1 text-base leading-relaxed rounded-lg font-bold border border-white relative z-10 peer" on:click={stop}>Stop</button>
-  {/if}
-  {#if state === STATE.PAUSED}
-  <button class="transition bg-white hover:bg-black hover:text-white text-black w-40 py-1 text-base leading-relaxed rounded-lg font-bold border border-white relative z-10 peer" on:click={resume}>Start</button>
-  {/if}
-  <div class="absolute inset-0 z-0 bg-gradient-to-r from-cyan-500 to-fuchsia-500 rounded-3xl blur-xl opacity-75"></div>
+	{#if state === STATE.NEW}
+		<button
+			class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
+			on:click={start}>Start</button
+		>
+	{/if}
+	{#if state === STATE.RUNNING}
+		<button
+			class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
+			on:click={stop}>Stop</button
+		>
+	{/if}
+	{#if state === STATE.PAUSED}
+		<button
+			class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
+			on:click={resume}>Start</button
+		>
+	{/if}
+	<div
+		class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-75 blur-xl"
+	/>
 </div>
 
-<div class="flex mt-8 w-3/4 max-w-sm md:max-w-screen-md md:min-w-[720px] justify-between">
-  <div class="flex flex-col items-center">
-    <div class="md:w-48 md:h-44 md:p-0 w-14 h-16 p-2 border border-neutral-700 rounded-lg flex items-center justify-center">
-      <h1 class="md:text-9xl text-3xl font-black">{h}</h1>
-    </div>
-    <h2 class="text-base font-medium leading-relaxed">H</h2>
-  </div>
-  <div class="flex flex-col items-center">
-    <div class="md:w-48 md:h-44 md:p-0 w-14 h-16 p-2 border border-neutral-700 rounded-lg flex items-center justify-center">
-      <h1 class="md:text-9xl text-3xl font-black">{m}</h1>
-    </div>
-    <h2 class="text-base font-medium leading-relaxed">M</h2>
-  </div>
-  <div class="flex flex-col items-center">
-    <div class="md:w-48 md:h-44 md:p-0 w-14 h-16 p-2 border border-neutral-700 rounded-lg flex items-center justify-center">
-      <h1 class="md:text-9xl text-3xl font-black">{s}</h1>
-    </div>
-    <h2 class="text-base font-medium leading-relaxed">S</h2>
-  </div>
-  <div class="flex flex-col items-center">
-    <div class="md:w-32 md:h-44 md:p-0 w-14 h-16 p-2 border border-neutral-700 rounded-lg flex box-border md:items-end items-center justify-center">
-      <h1 class="md:text-7xl text-3xl font-black md:pb-7">{ms}</h1>
-    </div>
-    <h2 class="text-base font-medium leading-relaxed">MS</h2>
-  </div>
+<div class="mt-8 flex w-3/4 max-w-sm justify-between md:min-w-[720px] md:max-w-screen-md">
+	<div class="flex flex-col items-center">
+		<div
+			class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
+		>
+			<h1 class="text-3xl font-black md:text-9xl">{h}</h1>
+		</div>
+		<h2 class="text-base font-medium leading-relaxed">H</h2>
+	</div>
+	<div class="flex flex-col items-center">
+		<div
+			class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
+		>
+			<h1 class="text-3xl font-black md:text-9xl">{m}</h1>
+		</div>
+		<h2 class="text-base font-medium leading-relaxed">M</h2>
+	</div>
+	<div class="flex flex-col items-center">
+		<div
+			class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
+		>
+			<h1 class="text-3xl font-black md:text-9xl">{s}</h1>
+		</div>
+		<h2 class="text-base font-medium leading-relaxed">S</h2>
+	</div>
+	<div class="flex flex-col items-center">
+		<div
+			class="box-border flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-32 md:items-end md:p-0"
+		>
+			<h1 class="text-3xl font-black md:pb-7 md:text-7xl">{ms}</h1>
+		</div>
+		<h2 class="text-base font-medium leading-relaxed">MS</h2>
+	</div>
 </div>
 
 {#if state === STATE.RUNNING || state === STATE.PAUSED}
-<button class="transition mt-8 bg-black text-neutral-700 w-40 py-1 text-base leading-relaxed rounded-lg font-bold border border-neutral-700 hover:text-white hover:border-white" on:click={reset}>Reset</button>
+	<button
+		class="mt-8 w-40 rounded-lg border border-neutral-700 bg-black py-1 text-base font-bold leading-relaxed text-neutral-700 transition hover:border-white hover:text-white"
+		on:click={reset}>Reset</button
+	>
 {/if}
 
-<p class="mt-24 text-sm font-normal text-center">© 2023 CheeseGrater<br/><a href="https://github.com/IncogNEET/StopWatch" class="text-blue-500 hover:underline">GitHub</a> | <a href="/colophon" class="text-blue-500 hover:underline">Colophon</a></p>
+<p class="mt-24 text-center text-sm font-normal">
+	© 2023 IncogNEET<br />
+	<a href="https://github.com/IncogNEET/StopWatch" class="text-blue-500 hover:underline">GitHub</a>
+	| <a href="/colophon" class="text-blue-500 hover:underline">Colophon</a>
+</p>
--- src/routes/colophon/+page.svelte
+++ src/routes/colophon/+page.svelte
@@ -1,28 +1,53 @@
 <script>
-  import icon from "$lib/assets/stopwatch.png";
-  import soyjacks from "$lib/assets/sveltesoyjacks.png";
+	import icon from '$lib/assets/stopwatch.png';
+	import soyjacks from '$lib/assets/sveltesoyjacks.png';
 </script>
+
 <a href="../">
-<div class="relative mt-16">
-  <img src={icon} alt="watch icon" class="w-24 box-content relative p-2 peer z-10"/>
-  <div class="absolute inset-0 z-0 bg-gradient-to-r from-cyan-500 to-fuchsia-500 rounded-3xl blur-xl opacity-50"></div>
-</div>
+	<div class="relative mt-16">
+		<img src={icon} alt="watch icon" class="peer relative z-10 box-content w-24 p-2" />
+		<div
+			class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-50 blur-xl"
+		/>
+	</div>
 </a>
 
-
-<a href="../"><h1 class="text-5xl font-black leading-none mt-6">StopWatch</h1></a>
+<a href="../"><h1 class="mt-6 text-5xl font-black leading-none">StopWatch</h1></a>
 <h2 class="text-base font-medium leading-relaxed">Colophon</h2>
 
-<section class="mt-12 text-sm font-normal sm:max-w-md w-3/4 leading-relaxed">
-  <p>This website was designed and developed by CheeseGrater in Japan.</p>
-  <p class="mt-4">It is built using <a href="https://code.visualstudio.com" class="text-blue-500 hover:underline">VS Code</a> with <a href="https://kit.svelte.dev" class="text-blue-500 hover:underline">SvelteKit</a> as the framework and <a href="https://tailwindcss.com" class="text-blue-500 hover:underline">TailwindCSS</a> as the front end, and deployed using <a href="https://vercel.com" class="text-blue-500 hover:underline">Vercel</a>.</p>
-  <p class="mt-4">It is designed in <a href="https://www.figma.com" class="text-blue-500 hover:underline">Figma</a> and the type is set in <a href="https://fonts.google.com/specimen/Inter" class="text-blue-500 hover:underline">Inter by Rasmus Andersson</a>.</p>
-  <p class="mt-4">The source code can be found in this <a href="https://github.com/IncogNEET/StopWatch" class="text-blue-500 hover:underline">GitHub repository</a>.</p>
+<section class="mt-12 w-3/4 text-sm font-normal leading-relaxed sm:max-w-md">
+	<p>This website was designed and developed by IncogNEET in Japan.</p>
+	<p class="mt-4">
+		It is built using <a href="https://code.visualstudio.com" class="text-blue-500 hover:underline"
+			>VS Code</a
+		>
+		with <a href="https://kit.svelte.dev" class="text-blue-500 hover:underline">SvelteKit</a> as the
+		framework and
+		<a href="https://tailwindcss.com" class="text-blue-500 hover:underline">TailwindCSS</a>
+		as the front end, and deployed using
+		<a href="https://vercel.com" class="text-blue-500 hover:underline">Vercel</a>.
+	</p>
+	<p class="mt-4">
+		It is designed in <a href="https://www.figma.com" class="text-blue-500 hover:underline">Figma</a
+		>
+		and the type is set in
+		<a href="https://fonts.google.com/specimen/Inter" class="text-blue-500 hover:underline"
+			>Inter by Rasmus Andersson</a
+		>.
+	</p>
+	<p class="mt-4">
+		The source code can be found in this <a
+			href="https://github.com/IncogNEET/StopWatch"
+			class="text-blue-500 hover:underline">GitHub repository</a
+		>.
+	</p>
 </section>
 
 <p class="mt-24">Thanks for visiting!</p>
-<div class="mt-4 relative">
-  <img src={soyjacks} alt="two soyjacks pointing svelte" class="relative w-56 rounded-xl z-10">
-  <div class="absolute inset-0 z-0 bg-gradient-to-r from-cyan-500 to-fuchsia-500 rounded-3xl blur-xl opacity-50"></div>
+<div class="relative mt-4">
+	<img src={soyjacks} alt="two soyjacks pointing svelte" class="relative z-10 w-56 rounded-xl" />
+	<div
+		class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-50 blur-xl"
+	/>
 </div>
 <p class="mt-4 mb-12">❤️</p>
