d928d9d4a2e40ceca282c9b619fdeba3c633cc4a
IncogNEET <incogneet@vivaldi.net>
2023-02-17 00:45:15 +0900

    added page transition animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { fade } from "svelte/transition";
+
 	import icon from '$lib/assets/stopwatch.png';
 
 	enum STATE {
@@ -57,84 +59,86 @@
 	};
 </script>
 
-<div class="relative mt-16">
-	<img src={icon} alt="watch icon" class="peer relative z-10 box-content w-24 p-2" />
-	<div
-		class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-50 blur-xl"
-	/>
-</div>
-
-<h1 class="mt-6 text-center text-5xl font-black leading-tight">StopWatch</h1>
-<h2 class="text-base font-medium leading-relaxed">Coolest Web Stopwatch</h2>
-
-<div class="relative mt-16">
-	{#if state === STATE.NEW}
+<div class="flex w-screen flex-col items-center" in:fade>
+	<div class="relative mt-16">
+		<img src={icon} alt="watch icon" class="peer relative z-10 box-content w-24 p-2" />
+		<div
+			class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-50 blur-xl"
+		/>
+	</div>
+	
+	<h1 class="mt-6 text-center text-5xl font-black leading-tight">StopWatch</h1>
+	<h2 class="text-base font-medium leading-relaxed">Coolest Web Stopwatch</h2>
+	
+	<div class="relative mt-16">
+		{#if state === STATE.NEW}
+			<button
+				class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
+				on:click={start}>Start</button
+			>
+		{/if}
+		{#if state === STATE.RUNNING}
+			<button
+				class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
+				on:click={stop}>Stop</button
+			>
+		{/if}
+		{#if state === STATE.PAUSED}
+			<button
+				class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
+				on:click={resume}>Start</button
+			>
+		{/if}
+		<div
+			class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-75 blur-xl"
+		/>
+	</div>
+	
+	<div class="mt-8 flex w-3/4 max-w-sm justify-between md:min-w-[720px] md:max-w-screen-md">
+		<div class="flex flex-col items-center">
+			<div
+				class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
+			>
+				<h1 class="text-3xl font-black md:text-9xl">{h}</h1>
+			</div>
+			<h2 class="text-base font-medium leading-relaxed">H</h2>
+		</div>
+		<div class="flex flex-col items-center">
+			<div
+				class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
+			>
+				<h1 class="text-3xl font-black md:text-9xl">{m}</h1>
+			</div>
+			<h2 class="text-base font-medium leading-relaxed">M</h2>
+		</div>
+		<div class="flex flex-col items-center">
+			<div
+				class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
+			>
+				<h1 class="text-3xl font-black md:text-9xl">{s}</h1>
+			</div>
+			<h2 class="text-base font-medium leading-relaxed">S</h2>
+		</div>
+		<div class="flex flex-col items-center">
+			<div
+				class="box-border flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-32 md:items-end md:p-0"
+			>
+				<h1 class="text-3xl font-black md:pb-7 md:text-7xl">{ms}</h1>
+			</div>
+			<h2 class="text-base font-medium leading-relaxed">MS</h2>
+		</div>
+	</div>
+	
+	{#if state === STATE.RUNNING || state === STATE.PAUSED}
 		<button
-			class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
-			on:click={start}>Start</button
+			class="mt-8 w-40 rounded-lg border border-neutral-700 bg-black py-1 text-base font-bold leading-relaxed text-neutral-700 transition hover:border-white hover:text-white"
+			on:click={reset}>Reset</button
 		>
 	{/if}
-	{#if state === STATE.RUNNING}
-		<button
-			class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
-			on:click={stop}>Stop</button
-		>
-	{/if}
-	{#if state === STATE.PAUSED}
-		<button
-			class="peer relative z-10 w-40 rounded-lg border border-white bg-white py-1 text-base font-bold leading-relaxed text-black transition hover:bg-black hover:text-white"
-			on:click={resume}>Start</button
-		>
-	{/if}
-	<div
-		class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-75 blur-xl"
-	/>
+	
+	<p class="mt-24 mb-12 text-center font-normal text-neutral-500">
+		© 2023 IncogNEET<br />
+		<a href="https://github.com/IncogNEET/StopWatch" class="text-blue-500 hover:underline">GitHub</a>
+		| <a href="/colophon" class="text-blue-500 hover:underline">Colophon</a>
+	</p>
 </div>
-
-<div class="mt-8 flex w-3/4 max-w-sm justify-between md:min-w-[720px] md:max-w-screen-md">
-	<div class="flex flex-col items-center">
-		<div
-			class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
-		>
-			<h1 class="text-3xl font-black md:text-9xl">{h}</h1>
-		</div>
-		<h2 class="text-base font-medium leading-relaxed">H</h2>
-	</div>
-	<div class="flex flex-col items-center">
-		<div
-			class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
-		>
-			<h1 class="text-3xl font-black md:text-9xl">{m}</h1>
-		</div>
-		<h2 class="text-base font-medium leading-relaxed">M</h2>
-	</div>
-	<div class="flex flex-col items-center">
-		<div
-			class="flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-48 md:p-0"
-		>
-			<h1 class="text-3xl font-black md:text-9xl">{s}</h1>
-		</div>
-		<h2 class="text-base font-medium leading-relaxed">S</h2>
-	</div>
-	<div class="flex flex-col items-center">
-		<div
-			class="box-border flex h-16 w-14 items-center justify-center rounded-lg border border-neutral-700 p-2 md:h-44 md:w-32 md:items-end md:p-0"
-		>
-			<h1 class="text-3xl font-black md:pb-7 md:text-7xl">{ms}</h1>
-		</div>
-		<h2 class="text-base font-medium leading-relaxed">MS</h2>
-	</div>
-</div>
-
-{#if state === STATE.RUNNING || state === STATE.PAUSED}
-	<button
-		class="mt-8 w-40 rounded-lg border border-neutral-700 bg-black py-1 text-base font-bold leading-relaxed text-neutral-700 transition hover:border-white hover:text-white"
-		on:click={reset}>Reset</button
-	>
-{/if}
-
-<p class="mt-24 mb-12 text-center font-normal text-neutral-500">
-	© 2023 IncogNEET<br />
-	<a href="https://github.com/IncogNEET/StopWatch" class="text-blue-500 hover:underline">GitHub</a>
-	| <a href="/colophon" class="text-blue-500 hover:underline">Colophon</a>
-</p>
--- src/routes/colophon/+page.svelte
+++ src/routes/colophon/+page.svelte
@@ -1,53 +1,58 @@
 <script>
+	import { fade } from 'svelte/transition';
+
 	import icon from '$lib/assets/stopwatch.png';
 	import soyjacks from '$lib/assets/sveltesoyjacks.png';
 </script>
 
-<a href="../">
-	<div class="relative mt-16">
-		<img src={icon} alt="watch icon" class="h- peer relative z-10 box-content h-[84px] w-24 p-2" />
+<div class="flex w-screen flex-col items-center" in:fade>
+	<a href="../">
+		<div class="relative mt-16">
+			<img src={icon} alt="watch icon" class="peer relative z-10 box-content w-24 p-2" />
+			<div
+				class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-50 blur-xl"
+			/>
+		</div>
+	</a>
+
+	<a href="../" class="text-center"
+		><h1 class="mt-6 text-5xl font-black leading-tight">StopWatch</h1></a
+	>
+	<h2 class="text-base font-medium leading-relaxed">Colophon</h2>
+
+	<section class="mt-12 w-3/4 text-base font-normal leading-relaxed text-neutral-500 sm:max-w-md">
+		<p>This website was designed and developed by IncogNEET in Japan.</p>
+		<p class="mt-4">
+			It is developed with
+			<a href="https://kit.svelte.dev" class="text-blue-500 hover:underline">SvelteKit</a>
+			and
+			<a href="https://tailwindcss.com" class="text-blue-500 hover:underline">TailwindCSS</a>, and
+			deployed using
+			<a href="https://vercel.com" class="text-blue-500 hover:underline">Vercel</a>.
+		</p>
+		<p class="mt-4">
+			It is designed in <a href="https://www.figma.com" class="text-blue-500 hover:underline"
+				>Figma</a
+			>
+			and the type is set in
+			<a href="https://fonts.google.com/specimen/Inter" class="text-blue-500 hover:underline"
+				>Inter by Rasmus Andersson</a
+			>.
+		</p>
+		<p class="mt-4">
+			The source code can be found in this <a
+				href="https://github.com/IncogNEET/StopWatch"
+				class="text-blue-500 hover:underline">GitHub repository</a
+			>.
+		</p>
+	</section>
+
+	<p class="mt-24">Thanks for visiting!</p>
+	<div class="relative mt-4">
+		<img src={soyjacks} alt="two soyjacks pointing svelte" class="relative z-10 w-56 rounded-xl" />
 		<div
-			class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-50 blur-xl"
+			class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-75 blur-xl"
 		/>
 	</div>
-</a>
-
-<a href="../" class="text-center"
-	><h1 class="mt-6 text-5xl font-black leading-none">StopWatch</h1></a
->
-<h2 class="text-base font-medium leading-relaxed">Colophon</h2>
-
-<section class="mt-12 w-3/4 text-base font-normal leading-relaxed text-neutral-500 sm:max-w-md">
-	<p>This website was designed and developed by IncogNEET in Japan.</p>
-	<p class="mt-4">
-		It is developed with
-		<a href="https://kit.svelte.dev" class="text-blue-500 hover:underline">SvelteKit</a>
-		and
-		<a href="https://tailwindcss.com" class="text-blue-500 hover:underline">TailwindCSS</a>, and
-		deployed using
-		<a href="https://vercel.com" class="text-blue-500 hover:underline">Vercel</a>.
-	</p>
-	<p class="mt-4">
-		It is designed in <a href="https://www.figma.com" class="text-blue-500 hover:underline">Figma</a
-		>
-		and the type is set in
-		<a href="https://fonts.google.com/specimen/Inter" class="text-blue-500 hover:underline"
-			>Inter by Rasmus Andersson</a
-		>.
-	</p>
-	<p class="mt-4">
-		The source code can be found in this <a
-			href="https://github.com/IncogNEET/StopWatch"
-			class="text-blue-500 hover:underline">GitHub repository</a
-		>.
-	</p>
-</section>
-
-<p class="mt-24">Thanks for visiting!</p>
-<div class="relative mt-4">
-	<img src={soyjacks} alt="two soyjacks pointing svelte" class="relative z-10 w-56 rounded-xl" />
-	<div
-		class="absolute inset-0 z-0 rounded-3xl bg-gradient-to-r from-cyan-500 to-fuchsia-500 opacity-75 blur-xl"
-	/>
+	<p class="mt-6 mb-12 text-neutral-500">© 2023 IncogNEET</p>
 </div>
-<p class="mt-4 mb-12 text-neutral-500">© 2023 IncogNEET</p>
